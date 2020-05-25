995b509081c6566c7464b52374e19cc130ff9028
crpistillo <cpistillo@fi.uba.ar>
2020-05-25 04:14:41 -0300

    generate_signed_url

--- src/services/file.service.js
+++ src/services/file.service.js
@@ -32,10 +32,14 @@ class FileService{
             });
         }
 
+        //TODO: Manejar excepciones
         async function get_metadata(file_name) {
             const [metadata] = await
                 bucket.file(file_name.toString()).getMetadata();
-                return {'file': metadata.name, 'size': metadata.size, 'updated': metadata.updated }
+
+            //TODO: Arreglar como se muestra en json
+            const url = generate_signed_url(file_name);
+            return {'file': metadata.name, 'size': metadata.size, 'updated': metadata.updated , 'url': url};
         }
 
 
@@ -50,6 +54,26 @@ class FileService{
                 }
             });
         }
+
+        //TODO: Manejar excepciones
+        //TODO: admitir videos
+        async function generate_signed_url(file_name) {
+            // These options will allow temporary read access to the file
+            const options = {
+                version: 'v2', // defaults to 'v2' if missing.
+                action: 'read',
+                expires: Date.now() + 1000 * 60 * 60, // one hour
+            };
+
+            // Get a v2 signed URL for the file
+            const [url] = await bucket.file(file_name).getSignedUrl(options);
+
+            console.log(url);
+
+            return url;
+
+        }
+
     }
 }
 
