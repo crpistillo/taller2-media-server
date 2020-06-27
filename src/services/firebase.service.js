e859663f850765df98be12dd53e5e5d8aa7a43a6
crpistillo <cpistillo@fi.uba.ar>
2020-06-27 05:21:30 -0300

    revert

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const bucketName = process.env.BUCKET_NAME;
 
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.CREDENTIALS)),
+    credential: admin.credential.cert(process.env.CREDENTIALS),
     storageBucket: bucketName
 });
 
