5b3af83197afb222587b99b131462c208c8f9e57
crpistillo <cpistillo@fi.uba.ar>
2020-06-27 05:15:14 -0300

    fix de prueba

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const bucketName = process.env.BUCKET_NAME;
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(process.env.CREDENTIALS),
     storageBucket: bucketName
 });
 
